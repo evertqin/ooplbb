94b60e2b2b21c216081b1ffc1599dd2822b11e37
Glenn Downing <downing@cs.utexas.edu>
2014-05-13 09:31:53 -0500

    another commit

--- examples/UnitTests.py
+++ examples/UnitTests.py
@@ -20,13 +20,13 @@ def cycle_length (n) :
 
 class UnitTests (TestCase) :
     def test_1 (self) :
-        self.assertTrue(cycle_length( 1) == 1)
+        self.assertEqual(cycle_length( 1), 1)
 
     def test_2 (self) :
-        self.assertTrue(cycle_length( 5) == 6)
+        self.assertEqual(cycle_length( 5), 6)
 
     def test_3 (self) :
-        self.assertTrue(cycle_length(10) == 7)
+        self.assertEqual(cycle_length(10), 7)
 
 main()
 
@@ -37,7 +37,7 @@ FAIL: test_1 (__main__.UnitTests)
 ----------------------------------------------------------------------
 Traceback (most recent call last):
   File "./UnitTests.py", line 23, in test_1
-    self.assertTrue(cycle_length( 1) == 1)
+    self.assertEqual(cycle_length( 1), 1)
   File "./UnitTests.py", line 18, in cycle_length
     assert c > 0
 AssertionError
@@ -47,16 +47,16 @@ FAIL: test_2 (__main__.UnitTests)
 ----------------------------------------------------------------------
 Traceback (most recent call last):
   File "./UnitTests.py", line 26, in test_2
-    self.assertTrue(cycle_length( 5) == 6)
-AssertionError: False is not true
+    self.assertEqual(cycle_length( 5), 6)
+AssertionError: 5 != 6
 
 ======================================================================
 FAIL: test_3 (__main__.UnitTests)
 ----------------------------------------------------------------------
 Traceback (most recent call last):
   File "./UnitTests.py", line 29, in test_3
-    self.assertTrue(cycle_length(10) == 7)
-AssertionError: False is not true
+    self.assertEqual(cycle_length(10), 7)
+AssertionError: 6 != 7
 
 ----------------------------------------------------------------------
 Ran 3 tests in 0.000s
