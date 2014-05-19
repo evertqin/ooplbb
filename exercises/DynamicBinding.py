dee1e240f89f1dc85e39ce5aa732c077ccbcd0ea
Glenn Downing <downing@cs.utexas.edu>
2014-05-18 22:29:16 -0500

    another commit

--- exercises/DynamicBinding.py
+++ exercises/DynamicBinding.py
@@ -5,35 +5,35 @@ What is the output of the following?
 """
 
 class A :
-	def f1 (self) :
-		return self.f2()
+    def f1 (self) :
+        return self.f2()
 
-	def f2 (self) :
-		return "A.f2"
+    def f2 (self) :
+        return "A.f2"
 
-	def g1 (self) :
-		return self.g2()
+    def g1 (self) :
+        return self.g2()
 
-	@staticmethod
-	def g2 () :
-		return "A.g2"
+    @staticmethod
+    def g2 () :
+        return "A.g2"
 
-	def h1 (self) :
-		return self.__h2()
+    def h1 (self) :
+        return self.__h2()
 
-	def __h2 (self) :
-		return "A.h2"
+    def __h2 (self) :
+        return "A.h2"
 
 class B (A) :
-	def f2 (self) :
-		return "B.f2"
+    def f2 (self) :
+        return "B.f2"
 
-	@staticmethod
-	def g2 () :
-		return "B.g2"
+    @staticmethod
+    def g2 () :
+        return "B.g2"
 
-	def __h2 (self) :
-		return "B.h2"
+    def __h2 (self) :
+        return "B.h2"
 
 print("DynamicBinding.py")
 
